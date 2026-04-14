71f5ec52ab87c0579bcfcc7390fab9cf8d6430dd
Solana Tabor <solanatabor@Solanas-MacBook-Air.local>
2026-04-14 17:28:51 -0500

    continued

--- STEP 1 Solana.R
+++ STEP 1 Solana.R
@@ -1,9 +1,12 @@
 library(readxl)
 source("./excel_date_time_utils.R")
 
+
 #Data Input ----
 step1 <- suppressWarnings(read_excel("./merged_diaries_actigraphy.xlsx"))
 View(step1)
+step1$`Sleep Onset Decimal Hour` <- as.numeric(step1$`Sleep Onset Decimal Hour`)
+step1$`Sleep Offset Time_Decimal Hour` <- as.numeric(step1$`Sleep Offset Time_Decimal Hour`)
 
 #making means for jules----
 library(dplyr)
@@ -17,3 +20,37 @@ install.packages("openxlsx")
 library(openxlsx)
 
 write.xlsx(means, "mean_per_person.xlsx")
+
+
+#Standard Deviations----
+library(dplyr)
+
+sleep_var <- step1 %>%
+  group_by(`Global Participant ID`) %>%
+  summarise(
+    onset_var = sd(step1$`Sleep Onset Decimal Hour`, na.rm = TRUE),
+    offset_var = sd(step1$`Sleep Offset Time_Decimal Hour`, na.rm = TRUE),
+    mean_alertness = mean(step1$`Alertness Rating`, na.rm = TRUE),
+    mean_wake_diff = mean(step1$`Wake Difficulty Rating`, na.rm = TRUE),
+    mean_anxiety = mean(step1$`Anxiety Rating`, na.rm = TRUE),
+    mean_sleep_quality = mean(step1$`Sleep Quality`, na.rm = TRUE)
+  )
+View(sleep_var)
+step1 <- merge(step1, sleep_var, by = "Global Participant ID")
+#Alertness Analysis----
+
+alert_onset <- lm(mean_alertness ~ onset_var, data = sleep_var)
+summary(alert_onset)
+alert_offset <- lm(mean_alertness ~ offset_var, data = sleep_var)
+summary(alert_offset)
+
+
+alert_onset_offset <- lm(step1$`Alertness Rating` ~ step1$`Sleep Offset Time_Decimal Hour` + 
+                                         step1$`Sleep Onset Decimal Hour` ,data = step1)
+summary(alert_onset_offset)
+
+model_alertness <- lm(step1$`Alertness Rating` ~ step1$`Sleep Duration` + 
+                                                 step1$`Sleep Offset Time_Decimal Hour` + 
+                                                 step1$`Sleep Onset Decimal Hour`, data = step1)
+summary(model_alertness)
+
